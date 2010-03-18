3e93f111b07ff4f65f9ff9980c0766b8e6c22388
Joshua Holt <holt.josh@gmail.com>
2010-03-17 22:49:12 -0400

    Adding pesudo code

--- packages/builder/lib/builder.js
+++ packages/builder/lib/builder.js
@@ -3,6 +3,19 @@ var seed     = require('seed'),
     markdown = seed.require('markdown'),
     Builder  = exports;
 
+/* 
+  pesudo code
+  1. gather all types being used to build virtual assets {
+    2. gather all assets of a type {
+      3. then build by passing the files through the respective parser
+    }(combineContentsIfPossible, writeToDisk)
+  }(loop)
+  (1) var storeVirtualAssetTypesAndNames = function(config, done) {}; 
+  (2) var findAssetsNeedingBuildForType = function(assets,done) {};
+  (3) var build = function(--,--) {}
+  
+  Co.chain(1,2,3)(--,--,--);
+*/
 Builder.build = function(assetType, done) {
   //TODO implement the build function
   return function(done) {
